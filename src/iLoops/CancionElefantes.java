05cf191b13cc85a7e8973215647a2d6b48891adb
Cr1cas890 <cricas@MacBook-Pro-de-Cristian.local>
2020-05-08 01:50:17 -0500

    elefantes

--- src/iLoops/CancionElefantes.java
+++ src/iLoops/CancionElefantes.java
@@ -5,13 +5,23 @@ public class CancionElefantes {
     public static void main(String[] args) {
 
         int numElefante = 0;
+        String cancion1 = "elefante se columpiaba sobre la tela de una araña, como veia que resistia, fue a llamar a otro elefante";
+        String cancion2 = "elefantes se columpiaban sobre la tela de una araña omo veian que resistia, fueron a llamar a otro elefante";
 
-        for (numElefante = 1; numElefante <=100; numElefante++){
-            if (numElefante == 1){
-                System.out.println(numElefante + " elefante se columpiaba sobre la tela de una araña, " +
-                        "como veia que resistia, fue a llamar a otro elefante");
-            }else System.out.println(numElefante + " elefantes se columpiaban sobre la tela de una araña," +
-                    "como veian que resistia, fueron a llamar a otro elefante");
+        for (numElefante = 1; numElefante <= 101; numElefante++) {
+
+            while (numElefante ==1)
+            {
+                System.out.println(numElefante + " " + " " + cancion1);
+                numElefante++;
+            }
+            if (numElefante <= 100)
+            {
+                System.out.println(numElefante + " " + " " + cancion2);
+            } else
+            {
+                System.out.println("Se rompio la telaraña");
+            }
         }
     }
 }
